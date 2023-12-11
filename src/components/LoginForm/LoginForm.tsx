206dcb6310219b1f1a88e28547a5536f8844ab85
stepan-mateiko <stepan-mateiko@gmail.com>
2023-12-11 16:50:24 +0200

    added login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
+
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+
 const LoginForm: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <div style={{ margin: "100px auto" }}>
+      <h2>Sign in</h2>
+      <p>Welcome back</p>
+      <form action="#" method="post">
+        <Input
+          type="text"
+          label="User name"
+          value=""
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
+        <Input
+          type="password"
+          label="Password"
+          value=""
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
+        <Button buttonText="Sign In" />
+      </form>
+      <p>OR</p>
+      <p>
+        Don't have account? <Link to="/registration">Sign in</Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginForm;
