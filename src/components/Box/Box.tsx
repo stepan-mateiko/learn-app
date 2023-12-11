4267cd837d2274ac5b1cd6f8747cbb00c040176a
stepan-mateiko <stepan-mateiko@gmail.com>
2023-12-12 00:54:24 +0200

    added base box component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,5 +1,21 @@
-const Box: React.FC = () => {
-  return <>Box</>;
+interface BoxProps {
+  imgSrc: string;
+  tag: string;
+  title: string;
+  date: string;
+  time: string;
+}
+const Box: React.FC<BoxProps> = ({ imgSrc, tag, title, date, time }) => {
+  return (
+    <div>
+      <img src={imgSrc} alt={`${title} box img`} />
+      <p>{tag}</p>
+      <h4>{title}</h4>
+      <p>
+        <span>{date}</span> <span>{`${time} min to read`}</span>
+      </p>
+    </div>
+  );
 };
 
 export default Box;
