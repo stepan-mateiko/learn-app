dd8671fea1d9550a8a6f65deeb3ede8f0de5c4e5
stepan-mateiko <stepan-mateiko@gmail.com>
2023-12-14 19:11:30 +0200

    updated mocked data

--- src/helpers/mockedTrainers.ts
+++ src/helpers/mockedTrainers.ts
@@ -1,5 +1,5 @@
 interface Trainer {
-  id: number;
+  id: string;
   username: string;
   lastName: string;
   firstName: string;
@@ -8,23 +8,12 @@ interface Trainer {
   specialization: string;
   photo: string;
   students: string[];
+  trainings: string[];
 }
-
-export const trainersHeadings: string[] = [
-  "ID",
-  "Username",
-  "Last Name",
-  "First Name",
-  "DOB",
-  "Address",
-  "Specialization",
-  "Photo",
-  "Students",
-];
 
 export const trainersData: Trainer[] = [
   {
-    id: 1,
+    id: "001",
     username: "trainer1",
     lastName: "Johnson",
     firstName: "Michael",
@@ -32,10 +21,11 @@ export const trainersData: Trainer[] = [
     address: "789 Elm St",
     specialization: "JavaScript",
     photo: "photo3.jpg",
-    students: ["student1", "student2"],
+    students: ["003", "005", "002"],
+    trainings: ["003", "004"],
   },
   {
-    id: 2,
+    id: "002",
     username: "trainer2",
     lastName: "Anderson",
     firstName: "Laura",
@@ -43,7 +33,103 @@ export const trainersData: Trainer[] = [
     address: "101 Pine St",
     specialization: "PHP",
     photo: "photo4.jpg",
-    students: ["student1", "student2"],
+    students: ["002"],
+    trainings: ["002"],
   },
-  // Add more trainer data
+  {
+    id: "003",
+    username: "trainer3",
+    lastName: "Williams",
+    firstName: "Jessica",
+    dob: "1991-09-15",
+    address: "222 Maple St",
+    specialization: "React",
+    photo: "photo5.jpg",
+    students: ["003", "004"],
+    trainings: ["001", "005"],
+  },
+  {
+    id: "004",
+    username: "trainer4",
+    lastName: "Davis",
+    firstName: "Ryan",
+    dob: "1987-12-03",
+    address: "333 Oak St",
+    specialization: "Python",
+    photo: "photo6.jpg",
+    students: ["005", "006"],
+    trainings: ["002", "004"],
+  },
+  {
+    id: "005",
+    username: "trainer5",
+    lastName: "Garcia",
+    firstName: "Mia",
+    dob: "1993-04-18",
+    address: "444 Birch St",
+    specialization: "Java",
+    photo: "photo7.jpg",
+    students: ["007", "008"],
+    trainings: ["005"],
+  },
+  {
+    id: "006",
+    username: "trainer6",
+    lastName: "Martinez",
+    firstName: "James",
+    dob: "1990-08-11",
+    address: "555 Pine St",
+    specialization: "C#",
+    photo: "photo8.jpg",
+    students: ["009", "001", "004"],
+    trainings: ["003"],
+  },
+  {
+    id: "007",
+    username: "trainer7",
+    lastName: "Clark",
+    firstName: "Sophie",
+    dob: "1989-06-28",
+    address: "666 Elm St",
+    specialization: "Ruby",
+    photo: "photo9.jpg",
+    students: ["001", "002", "007"],
+    trainings: ["002"],
+  },
+  {
+    id: "008",
+    username: "trainer8",
+    lastName: "Miller",
+    firstName: "David",
+    dob: "1994-02-14",
+    address: "777 Birch St",
+    specialization: "Angular",
+    photo: "photo10.jpg",
+    students: ["003", "004"],
+    trainings: ["004"],
+  },
+  {
+    id: "009",
+    username: "trainer9",
+    lastName: "Anderson",
+    firstName: "Emma",
+    dob: "1985-11-22",
+    address: "888 Oak St",
+    specialization: "Vue.js",
+    photo: "photo11.jpg",
+    students: ["006"],
+    trainings: ["005"],
+  },
+  {
+    id: "010",
+    username: "trainer10",
+    lastName: "Johnson",
+    firstName: "Daniel",
+    dob: "1992-06-15",
+    address: "999 Pine St",
+    specialization: "Swift",
+    photo: "photo12.jpg",
+    students: ["007", "008"],
+    trainings: ["001", "004"],
+  },
 ];
--- src/helpers/mockedTrainings.ts
+++ src/helpers/mockedTrainings.ts
@@ -1,14 +1,3 @@
-interface Training {
-  id: number;
-  duration: string;
-  trainer: string;
-  type: string;
-  date: string;
-  name: string;
-  description: string;
-  students: string[];
-}
-
 export const trainingsHeadings: string[] = [
   "Date",
   "Name",
@@ -18,26 +7,116 @@ export const trainingsHeadings: string[] = [
   "Duration",
 ];
 
+interface Training {
+  id: string;
+  duration: number;
+  trainer: string;
+  type: string;
+  date: string;
+  name: string;
+  description: string;
+  students: string[];
+}
+
 export const trainingsData: Training[] = [
   {
-    id: 1,
-    duration: "1 hour",
-    trainer: "trainer1",
-    type: "Fitness",
+    id: "001",
+    duration: 60,
+    trainer: "004",
+    type: "Full course",
     date: "2023-01-10",
-    name: "Training 1",
-    description: "Fitness training for beginners",
-    students: ["student1"],
+    name: "JavaScript",
+    description: "JavaScript training for beginners",
+    students: ["005"],
   },
   {
-    id: 2,
-    duration: "45 minutes",
-    trainer: "trainer2",
-    type: "Yoga",
+    id: "002",
+    duration: 45,
+    trainer: "007",
+    type: "Webinar",
     date: "2023-02-15",
-    name: "Training 2",
-    description: "Yoga for flexibility",
-    students: ["student1", "student2"],
+    name: "GraphQL",
+    description: "GraphQL for developers",
+    students: ["007", "002"],
   },
-  // Add more training data
+  {
+    id: "003",
+    duration: 90,
+    trainer: "002",
+    type: "Workshop",
+    date: "2023-03-20",
+    name: "React Hooks",
+    description: "Mastering React Hooks",
+    students: ["004", "008"],
+  },
+  {
+    id: "004",
+    duration: 75,
+    trainer: "009",
+    type: "Full course",
+    date: "2023-04-25",
+    name: "Python Fundamentals",
+    description: "Python fundamentals for beginners",
+    students: ["001", "003", "009"],
+  },
+  {
+    id: "005",
+    duration: 120,
+    trainer: "005",
+    type: "Bootcamp",
+    date: "2023-05-30",
+    name: "Java Programming",
+    description: "Intensive Java programming bootcamp",
+    students: ["006", "010"],
+  },
+  {
+    id: "006",
+    duration: 60,
+    trainer: "003",
+    type: "Webinar",
+    date: "2023-06-15",
+    name: "C# Basics",
+    description: "Understanding the basics of C#",
+    students: ["007", "001"],
+  },
+  {
+    id: "007",
+    duration: 90,
+    trainer: "006",
+    type: "Workshop",
+    date: "2023-07-20",
+    name: "Ruby on Rails",
+    description: "Building web applications with Ruby on Rails",
+    students: ["002", "005", "008"],
+  },
+  {
+    id: "008",
+    duration: 45,
+    trainer: "010",
+    type: "Webinar",
+    date: "2023-08-25",
+    name: "Swift for iOS",
+    description: "Introduction to Swift for iOS development",
+    students: ["004", "006"],
+  },
+  {
+    id: "009",
+    duration: 75,
+    trainer: "008",
+    type: "Full course",
+    date: "2023-09-30",
+    name: "Vue.js Essentials",
+    description: "Essential concepts of Vue.js framework",
+    students: ["007", "001"],
+  },
+  {
+    id: "010",
+    duration: 120,
+    trainer: "004",
+    type: "Bootcamp",
+    date: "2023-10-15",
+    name: "Angular Advanced",
+    description: "Advanced topics in Angular development",
+    students: ["005", "009"],
+  },
 ];
