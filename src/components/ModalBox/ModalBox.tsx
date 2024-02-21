e72f86d55261439bf91268889c6bebd3a7d30355
stepan-mateiko <stepan-mateiko@gmail.com>
2024-02-21 22:03:33 +0200

    added styles for modal

--- src/components/ModalBox/ModalBox.tsx
+++ src/components/ModalBox/ModalBox.tsx
@@ -30,6 +30,7 @@ const ModalBox: React.FC<ModalBoxProps> = ({
       isOpen={isModalOpen}
       onRequestClose={handleModalClose}
       style={{ overlay: { zIndex: 10 } }}
+      className="delete"
     >
       <h3>Profile Deletion Confirmation</h3>
       <p>
@@ -37,13 +38,17 @@ const ModalBox: React.FC<ModalBoxProps> = ({
         profile, we want you to know that this action is permanent and
         irreversible. You'll lose access to all your account information, course
         progress, certificates, and any learning communities you're a part of.
+      </p>
+      <p>
         If there's anything we can do to improve your experience or if you need
         assistance with any issues you've encountered, please reach out to our
-        support team. We're always here to help. If you still wish to delete
-        your account, please click on the 'Confirm' button below.
+        support team. We're always here to help.
       </p>
-
-      <div>
+      <p>
+        If you still wish to delete your account, please click on the 'Confirm'
+        button below.
+      </p>
+      <div className="delete__btns">
         <Button onClick={handleModalClose} buttonText="Cancel" />
         <Button buttonText="Confirm" onClick={deleteAccount} />
       </div>
